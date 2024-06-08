be0de7490efed1b1c4229ee09cf8502fdc74df62
perterHUAN <47806541+perterHUAN@users.noreply.github.com>
2024-06-08 20:18:55 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Grid from "./components/grid.vue";
+</script>
+
+<template>
+  <div class="grid place-content-center">
+    <div class="font-bold text-2xl text-center">Hello World</div>
+    <Grid class="select-none"/>
+  </div>
+</template>
---
+++ src/components/grid.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const colors = ["","bg-slate-100", "bg-slate-200", "bg-slate-300"];
+</script>
+<template>
+  <div class="flex flex-col gap-1">
+    <div v-for="row in 10" class="flex gap-1">
+      <div v-for="col in 10" class="w-6 h-6 border border-gray-400"></div>
+    </div>
+  </div>
+  <el-row>
+    <el-col v-for="idx in 3" :span="8" :class="colors[idx]">你好</el-col>
+  </el-row>
+</template>
+<style scoped></style>
