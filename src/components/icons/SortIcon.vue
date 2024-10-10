cfb7b15f6642eb5b67f62da36cfdf3bc7a18cc56
karpenko_a <karpenko_a@lave.tech>
2024-10-10 21:47:40 +0300

    Добавлены иконки

---
+++ src/components/icons/SortIcon.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5.6665 12.5H10.6665V15H5.6665V12.5ZM5.6665 8.75H13.1665V11.25H5.6665V8.75ZM5.6665 5H15.6665V7.5H5.6665V5Z"
+          fill="#6B6D80"/>
+  </svg>
+
+</template>
