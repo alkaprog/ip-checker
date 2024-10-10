ee4317dfe585ca0240617a05569dd7eb0a9815ef
karpenko_a <karpenko_a@lave.tech>
2024-10-10 21:52:08 +0300

    Убрал лишнее из layout'а. Добавил потерянные стили. Удалил ненужный компонент

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,12 @@ import Grid from "./components/grid.vue";
 
 <template>
   <el-config-provider namespace="ep">
-    <div class="h- w-screen flex">
-      <el-scrollbar>
+    <div class="h-screen w-full">
         <el-main class="px-[120px] py-[80px]">
           <router-view v-slot="{ Component, route }">
-            <transition name="el-fade-in-linear">
-              <component :is="Component" :key="route.path"/>
-            </transition>
+            <component :is="Component" :key="route.path"/>
           </router-view>
         </el-main>
-      </el-scrollbar>
     </div>
   </el-config-provider>
 </template>
